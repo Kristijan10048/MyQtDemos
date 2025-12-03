a2c627b8c59600cf25355409b3eb07cfabccbe09
Kristijan <kristijan.petkov@gmail.com>
2025-12-03 13:20:56 +0100

    Initial implementaiion

---
+++ keword-StaticDemo/ctest.cpp
@@ -0,0 +1,8 @@
+#include "ctest.h"
+#include <iostream>
+
+using namespace std;
+
+CAppTest::CAppTest() {
+    cout<< "CAppConstructor" <<endl;
+}
---
+++ keword-StaticDemo/ctest.h
@@ -0,0 +1,10 @@
+#ifndef CTEST_H
+#define CTEST_H
+
+class CAppTest
+{
+public:
+    CAppTest();
+};
+
+#endif // CTEST_H
