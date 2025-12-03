2fca4351022041c64d047161c97b7de98da78a70
Kristijan <kristijan.petkov@gmail.com>
2025-12-03 16:22:34 +0100

    Included a test class. Correct commit

---
+++ Keyword-Static-Demo/CTest.cpp
@@ -0,0 +1,14 @@
+#include "CTest.h"
+#include <iostream>
+
+using namespace std;
+
+CAppTest::CAppTest() {
+
+    cout << "AppTest Constructor!" << endl;
+}
+
+CAppTest::~CAppTest()
+{
+    cout << "AppTest Destrucotr!" << endl;
+}
---
+++ Keyword-Static-Demo/CTest.h
@@ -0,0 +1,11 @@
+#ifndef CTEST_H
+#define CTEST_H
+
+class CAppTest
+{
+public:
+    CAppTest();
+    ~CAppTest();
+};
+
+#endif // CTEST_H
